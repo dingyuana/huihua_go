2f7e45de28aab46c0430568cd59eb92e856562e6
Developer <dev@huihua.local>
2026-05-30 00:28:16 +0800

    chore: clean up temp files

--- web-tmp/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<script setup lang="ts">
-// 根组件 - 路由顶层出口，布局由路由 meta.layout 控制
-</script>
--- web-tmp/src/types/element.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'element-plus/dist/locale/zh-cn.mjs' {
-  import type { Language } from 'element-plus/es/locale'
-  const zhCn: Language
-  export default zhCn
-}
